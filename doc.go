9fb4101dd53507b91eac003ef95715c55224fba1
Eric Sammer <esammer@gmail.com>
2021-08-08 17:06:32 -0700

    Updated file copyright headers.

--- doc.go
+++ doc.go
@@ -1,3 +1,17 @@
+// Copyright 2021 Eric Sammer
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package go_tasks is a small, zero dependency, library that provides a production-ready implementation of common
 // concurrent task management patterns. It's suitable for implementing fine-grained worker pools. It's meant to make
 // task implementations easily testable without adding a ton of framework or concurrency infrastructure.
